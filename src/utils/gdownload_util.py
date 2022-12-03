4b463a96c934958eb1063af377e68e259203fff2
canaleal <alexcanales766@gmail.com>
2022-12-03 17:25:46 -0500

    Added try catches to the gdownloader util

--- src/utils/gdownload_util.py
+++ src/utils/gdownload_util.py
@@ -7,16 +7,22 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
 def get_file_id_from_raw_url(raw_url):
-    result = re.search('https://drive.google.com/file/d/(.*)', raw_url)
-    file_id = re.sub('/view\?usp=sharing', '', result.group(1))
-    return file_id
+    try:
+        result = re.search('https://drive.google.com/file/d/(.*)', raw_url)
+        file_id = re.sub('/view\?usp=sharing', '', result.group(1))
+        return file_id
+    except Exception as e:
+        print(f"An error occurred while getting the file ID from the raw URL: {e}")
 
 # * String the id from the original link and add it to the new system
 def download_drive_file(raw_url, save_path_url):
-    file_id = get_file_id_from_raw_url(raw_url)
-    clean_url = f'https://www.googleapis.com/drive/v3/files/{file_id}?alt=media&key={API_KEY}'
-    gdown.download(clean_url, save_path_url, quiet=False)
-    return file_id
+    try:
+        file_id = get_file_id_from_raw_url(raw_url)
+        clean_url = f'https://www.googleapis.com/drive/v3/files/{file_id}?alt=media&key={API_KEY}'
+        gdown.download(clean_url, save_path_url, quiet=False)
+        return file_id
+    except Exception as e:
+        print(f"An error occurred while downloading the file from Google Drive: {e}")
 
 def get_raw_geojson_from_file(save_path_url):
     """load the geojson data from the coordinates file downloaded from google drive.
@@ -26,6 +32,9 @@ def get_raw_geojson_from_file(save_path_url):
     Returns:
         geojson: json data formated with geojson data types and feature arrays
     """
-    f = open(save_path_url)
-    raw_geojson_data = json.load(f)
-    return raw_geojson_data
+    try:
+        f = open(save_path_url)
+        raw_geojson_data = json.load(f)
+        return raw_geojson_data
+    except Exception as e:
+        print(f"An error occurred while getting the raw GeoJSON from the file: {e}")
